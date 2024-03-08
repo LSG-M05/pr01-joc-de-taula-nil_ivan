3e572695c746b7c71813fbda1c2e8ee968c3d453
--nilmarquez <--nilmarquezr@gmail.com>
2024-03-08 10:28:21 +0100

    Segundo menú + Declaración de comunidades autónomas

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,56 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Random;
 public class Main {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
 
-            boolean salir = false;
-            while (!salir) {
-                menu();
-                int opcion = scanner.nextInt();
-                switch (opcion) {
+    Scanner input = new Scanner(System.in);
+
+    public static int jugadores = 0;
+
+    //Declaración variables comunidades autónomas
+    public static int galicia = 2;
+    public static int asturias = 2;
+    public static int cantabria = 2;
+    public static int pais_vasco = 2;
+    public static int navarra = 2;
+    public static int la_rioja = 2;
+    public static int aragon = 2;
+    public static int cataluña = 2;
+    public static int castilla_y_leon = 2;
+    public static int madrid = 2;
+    public static int castilla_la_mancha = 2;
+    public static int extremadura = 2;
+    public static int andalucia = 2;
+    public static int murcia = 2;
+    public static int canatias = 2;
+    public static int baleares = 2;
+
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        main.init();
+
+    }
+
+    public void init() {
+        int menuItem = 0;
+        do {
+            System.out.println("1. Agregar jugador");
+            System.out.println("2. Asignar territorios");
+            System.out.println("3. Turno del jugador");
+            System.out.println("4. Estado actual");
+            System.out.println("5. Invadir territorio");
+            System.out.println("6. Ganar territorio");
+            System.out.println("7. Eliminar territorio");
+            System.out.println("8. Ganar partida");
+            System.out.println("9. Invadir francia");
+            System.out.println("10. Salir");
+            System.out.print("Ingrese la opción: ");
+
+            if (input.hasNextInt()) {
+                menuItem = input.nextInt();
+                switch (menuItem) {
                     case 1:
                         ordenjugadores();
                         break;
@@ -39,34 +77,32 @@ public class Main {
                         break;
                     case 9:
                         invadirfrancia();
+                        break;
                     case 10:
-                        salir = true;
                         break;
                     default:
                         System.out.println("Opción no válida");
-                        break;
                 }
+            }else{
+                System.out.println("Opción no valida");
             }
-        }
+            input.nextLine();
+            System.out.println("");
 
-        private static void menu() {
-            System.out.println("1. Agregar jugador");
-            System.out.println("2. Asignar territorios");
-            System.out.println("3. Turno del jugador");
-            System.out.println("4. Estado actual");
-            System.out.println("5. Invadir territorio");
-            System.out.println("6. Ganar territorio");
-            System.out.println("7. Eliminar territorio");
-            System.out.println("8. Ganar partida");
-            System.out.println("9. Invadir francia");
-            System.out.println("10. Salir");
-            System.out.print("Ingrese la opción: ");
-        }
-
-    private static void ordenjugadores(){
+        }while(menuItem!=10);
 
     }
+    private static void ordenjugadores(){
+        //code ivan
+    }
     private static void asignacionterritorios(){
+        if (jugadores == 0) {
+            System.out.println("No hay jugadores para asignar territorios");
+            return;
+        }else{
+
+        }
+        //code nil
 
     }
     private static void turnojugador(){
