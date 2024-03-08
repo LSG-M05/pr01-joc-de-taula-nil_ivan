37ac7b6721e8e0dd4254471c23702adbea283dab
Ivan <ivalleciura@gmail.com>
2024-03-08 10:35:53 +0100

    08/03/2024/

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,26 @@ import java.util.Random;
 // Project moved to another repositori together with classmates
 //Risk Game
 public class Main {
+    String[] jugadors = new String[4];
     Scanner input = new Scanner(System.in);
     private static void menu(){
 
 }
     private static void ordenjugadores(){
+        Random random = new Random();
+        int dau = 0;
+        String[] x = new String[4];
+        int[] daus = new int[4];
+        for (int i=0; i<4; i++) {
+            System.out.println("Escriu el nom de un jugador");
+        }
+        System.out.println("Ara es tiraran els daus per decidir l'ordre");
 
+        for(int i=0; i<4; i++) {
+            dau = random.nextInt();
+            for(int j=0; j<4; j++) {
+            }
+        }
     }
     private static void asignacionterritorios(){
 
